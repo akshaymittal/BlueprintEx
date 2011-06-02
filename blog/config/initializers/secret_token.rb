d716ff9d6aadad76d9a5ca1e124448da0aba8bb2
akshaymittal <akshay@golygon.com>
2011-06-03 00:07:30 +0530

    Adding the Blog App from ruby on rails guides Some error that i cannot figure out

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ab5376941fcf3280358c0e47f48481455d9334ce994b79ff77c1755342869d85c2c6703959ceb8e4989d2caa5bab3496379aaa48a929e09468b990501efd748b'
